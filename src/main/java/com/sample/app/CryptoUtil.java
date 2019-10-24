fd4ad7cb0f3e3ceeb36ad103049dc9a2b09de5b7
jakekim84 <jakekim84@KIM-DOJIN>
2019-10-24 15:43:35 +0900

    변경

--- src/main/java/com/sample/app/CryptoUtil.java
+++ src/main/java/com/sample/app/CryptoUtil.java
@@ -96,11 +96,11 @@ public class CryptoUtil {
     public static void main(String[] args) throws Exception {
         //String plainText = "Hello, World!";
 
-        String key = "sampleKey";
 
         //System.out.println("MD5     : " + plainText + " - " + CryptoUtil.md5(plainText));
         //System.out.println("SHA-256 : " + plainText + " - " + CryptoUtil.sha256(plainText));
 
+        String key = "sampleKey";
         String encrypted = CryptoUtil.encryptAES256("rlaehwls", key);
         System.out.println("AES-256 : enc - " + encrypted);
         System.out.println("AES-256 : dec - " + CryptoUtil.decryptAES256(encrypted, key));
